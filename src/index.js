17e096b5fa87e89437a292da94450fdac56edce6
Anders Mortensen <anders@sondberg.dk>
2026-05-19 12:15:28 +0200

    added test page /mypage

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+app.get('/mypage', (req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end('<h1>Welcome to My Page!</h1>');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
